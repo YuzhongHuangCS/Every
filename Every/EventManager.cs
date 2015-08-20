c40bf6cb1df65b97875937f6f43b1c0c564a54c0
Yuzo <hyzgog@gmail.com>
2015-08-20 10:32:36 +0800

    basic EventManager

--- Every/EventManager.cs
+++ Every/EventManager.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Every {
     class EventManager {
-        public EventManager(string path) {
+        private FileSystemWatcher watcher;
 
+        public EventManager(string path) {
+            watcher = new FileSystemWatcher();
+            watcher.Filter = Path.GetFileName(path);
+            watcher.Path = Path.GetDirectoryName(path);
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnDeleted);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnCreated(object sender, FileSystemEventArgs e) {
+            Console.WriteLine(string.Format("[{0}] {1}:{2}", DateTime.Now.ToString(), e.ChangeType.ToString(), e.FullPath));
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e) {
+            Console.WriteLine(string.Format("[{0}] {1}:{2}", DateTime.Now.ToString(), e.ChangeType.ToString(), e.FullPath));
+        }
+
+        private void OnDeleted(object sender, FileSystemEventArgs e) {
+            Console.WriteLine(string.Format("[{0}] {1}:{2}", DateTime.Now.ToString(), e.ChangeType.ToString(), e.FullPath));
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e) {
+            Console.WriteLine(string.Format("[{0}] {1}:{2}", DateTime.Now.ToString(), e.ChangeType.ToString(), e.FullPath));
         }
     }
 }
