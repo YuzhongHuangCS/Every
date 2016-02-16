63d7db52c1cfb2f6186c0ddfc1ab3cbc9d9de774
PillowSky <pillowsky@qq.com>
2016-02-16 15:28:25 +0800

    restart with WinForms

---
+++ FormMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Every {
+    partial class FormMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
+            this.listViewMain = new System.Windows.Forms.ListView();
+            this.buttonAddRepo = new System.Windows.Forms.Button();
+            this.tableLayoutPanelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanelMain
+            // 
+            this.tableLayoutPanelMain.ColumnCount = 1;
+            this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelMain.Controls.Add(this.listViewMain, 0, 0);
+            this.tableLayoutPanelMain.Controls.Add(this.buttonAddRepo, 0, 1);
+            this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
+            this.tableLayoutPanelMain.RowCount = 2;
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelMain.Size = new System.Drawing.Size(832, 469);
+            this.tableLayoutPanelMain.TabIndex = 0;
+            // 
+            // listViewMain
+            // 
+            this.listViewMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewMain.Location = new System.Drawing.Point(4, 4);
+            this.listViewMain.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listViewMain.Name = "listViewMain";
+            this.listViewMain.Size = new System.Drawing.Size(824, 367);
+            this.listViewMain.TabIndex = 0;
+            this.listViewMain.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonAddRepo
+            // 
+            this.buttonAddRepo.Location = new System.Drawing.Point(4, 379);
+            this.buttonAddRepo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonAddRepo.Name = "buttonAddRepo";
+            this.buttonAddRepo.Size = new System.Drawing.Size(824, 86);
+            this.buttonAddRepo.TabIndex = 1;
+            this.buttonAddRepo.Text = "Add Repository";
+            this.buttonAddRepo.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 469);
+            this.Controls.Add(this.tableLayoutPanelMain);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormMain";
+            this.Text = "Every";
+            this.tableLayoutPanelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
+        private System.Windows.Forms.ListView listViewMain;
+        private System.Windows.Forms.Button buttonAddRepo;
+    }
+}
